f3358134469968c2be3906d10ed698a8c9972de3
MOGESH.M <143895303+Mogeshaue@users.noreply.github.com>
2025-11-30 22:28:25 +0530

    Refactor script.js to comment out unused interval and timeout code; add CAN bus technical report

--- event-driven/script.js
+++ event-driven/script.js
@@ -14,13 +14,23 @@ button.addEventListener("mouseleave", function() {
   clearTimeout(timeoutId); // Tooltip will not show if mouse leaves early
 });
 
-let count = 1;
-let intervalId = setInterval(function() {
-  console.log("Count: " + count);
-  count++;
-}, 1000);
+// let count = 1;
+// let intervalId = setInterval(function() {
+//   console.log("Count: " + count);
+//   count++;
+// }, 1000);
 
-setTimeout(function() {
-  clearInterval(intervalId);
-  console.log("Stopped counting");
-}, 50000);
+// setTimeout(function() {
+//   clearInterval(intervalId);
+//   console.log("Stopped counting");
+// }, 50000);
+
+// let id=setInterval(function(){
+//   console.log('Counting...')
+//     console.log(id)
+// },1000)
+
+// // console.log(id)
+// setInterval(function(){
+// clearInterval(id)
+// },4000)
